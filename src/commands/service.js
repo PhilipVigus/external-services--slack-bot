2006ca20b98768e661656374adc2e66484d5f6ad
PhilipVigus <philipvigus@yahoo.co.uk>
2021-11-14 12:58:49 +0000

    Simplify async code

--- src/commands/service.js
+++ src/commands/service.js
@@ -9,77 +9,60 @@ const initialise = () => {
     });
 };
 
+const fetchServiceStatus = async (serviceName) => {
+    const service = services[serviceName];
+    const status = await service.getStatus();
+
+    return {
+        name: serviceName,
+        status,
+    };
+};
+
+const fetchAllServiceStatuses = async () => {
+    const serviceStatuses = Object.keys(services).map(async (serviceName) =>
+        fetchServiceStatus(serviceName)
+    );
+
+    return Promise.all(serviceStatuses);
+};
+
+const formatStatuses = (serviceStatuses) => {
+    const responses = serviceStatuses.map((status) => {
+        const service = services[status.name];
+
+        return {
+            markdown: `${service.title} - <${service.statusPage}|${status.status}>`,
+            text: `${service.title} - ${status.status}`,
+        };
+    });
+
+    const plain = responses.map((response) => response.text).join("\n");
+
+    const markdown = responses.map((response) => response.markdown).join("\n");
+
+    return {
+        blocks: [
+            {
+                type: "section",
+                text: {
+                    type: "mrkdwn",
+                    text: markdown,
+                },
+            },
+        ],
+        text: plain,
+    };
+};
+
 const register = (app) => {
-    app.command("/service", async ({ command, ack, say }) => {
+    app.command("/service", async ({ ack, say }) => {
         await ack();
 
-        if (command.text === "") {
-            await say("You must provide a service name");
-            return;
-        }
+        const serviceStatuses = await fetchAllServiceStatuses();
+        const formattedResponse = formatStatuses(serviceStatuses);
 
-        if (!(command.text in services) && command.text !== "all") {
-            await say("Unrecognised service name");
-            return;
-        }
-
-        if (command.text === "all") {
-            const serviceStatuses = await Promise.all(
-                Object.keys(services).map(async (serviceName) => {
-                    const service = services[serviceName];
-                    const status = await service.getStatus();
-
-                    return {
-                        markdownResponse: `${service.title} - <${service.statusPage}|${status}>`,
-                        plainResponse: `${service.title} - ${status}`,
-                    };
-                })
-            );
-
-            const plainResponse = serviceStatuses
-                .map((serviceStatus) => serviceStatus.plainResponse)
-                .join("\n");
-
-            const markdownResponse = serviceStatuses
-                .map((serviceStatus) => serviceStatus.markdownResponse)
-                .join("\n");
-
-            await say({
-                blocks: [
-                    {
-                        type: "section",
-                        text: {
-                            type: "mrkdwn",
-                            text: markdownResponse,
-                        },
-                    },
-                ],
-                text: plainResponse,
-            });
-
-            return;
-        }
-
-        const service = services[command.text];
-
-        const status = await service.getStatus();
-
-        const markdownResponse = `${service.title} - <${service.statusPage}|${status}>`;
-
-        const plainResponse = `${service.title} - ${status}`;
-
-        await say({
-            blocks: [
-                {
-                    type: "section",
-                    text: {
-                        type: "mrkdwn",
-                        text: markdownResponse,
-                    },
-                },
-            ],
-            text: plainResponse,
-        });
+        await say(formattedResponse);
     });
 };
 
